4a2ccccf0b2c351028647dcecf12b5a242c88e9e
Yorki03 <110152632+Yorki03@users.noreply.github.com>
2023-07-29 13:54:47 +0200

    organización de los componentes y creación de la primera pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+const newLocal = './peges/informacion/informacion.module';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./peges/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'informacion',
+    loadChildren: () => import('./peges/informacion/informacion.module').then( m => m.InformacionPageModule)
   },
 ];
 
