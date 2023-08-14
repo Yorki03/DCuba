5a3489e37e73f63ccc54973fc7724f96e97c1978
christian <christianre917@gmail.com>
2023-08-14 16:54:29 -0400

    nueva actualizacion

--- src/app/pages/operaciones/components/jugadas/jugadas.page.ts
+++ src/app/pages/operaciones/components/jugadas/jugadas.page.ts
@@ -12,4 +12,19 @@ export class JugadasPage implements OnInit {
   ngOnInit() {
   }
 
+  opcion1: boolean = true;
+  opcion2: boolean = false;
+
+  opcion1Changed() {
+    if (this.opcion1) {
+      this.opcion2 = false;
+    }
+  }
+
+  opcion2Changed() {
+    if (this.opcion2) {
+      this.opcion1 = false;
+    }
+  }
+
 }
