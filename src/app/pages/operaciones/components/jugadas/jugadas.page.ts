6eb4973498b605adf86b8bec5523aa141609db48
christian <christianre917@gmail.com>
2023-08-15 21:11:32 -0400

    pagina jugadas completa

--- src/app/pages/operaciones/components/jugadas/jugadas.page.ts
+++ src/app/pages/operaciones/components/jugadas/jugadas.page.ts
@@ -27,4 +27,10 @@ export class JugadasPage implements OnInit {
     }
   }
 
+  validateInput(event: any) {
+    const inputValue = event.target.value;
+    const cleanedValue = inputValue.replace(/[^0-9]/g, ''); // Eliminar no números
+    const limitedValue = cleanedValue.substring(0, 2); // Limitar a dos caracteres
+    event.target.value = limitedValue; // Actualizar el valor en el campo de entrada
+  }
 }
