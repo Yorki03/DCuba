d13b9028b04d58f9dca9b2e1bac1d6f56068a91c
Rodolfo <rodolvg87@gmail.com>
2023-09-18 20:45:15 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.d-cuba',
   appName: 'dcuba',
   webDir: 'www',
   server: {
